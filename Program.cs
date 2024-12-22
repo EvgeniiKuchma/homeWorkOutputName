2141e3536d7b465bcd6ceff70a450db57c8a117e
Евгений Кучма <desferto87@gmail.com>
2024-12-22 21:37:40 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace homeWorkOutputName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberString = 3;
+            int sizeNumberSimbol = 2;
+            string inputUserName;
+            char inputUserSimbol;
+
+            Console.Write("Enter the desired name: ");
+            inputUserName = Console.ReadLine();
+            Console.Write("Enter the desired simbol: ");
+            inputUserSimbol = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i < numberString; i++)
+            {
+                if (i == numberString - sizeNumberSimbol)
+                {
+                    Console.WriteLine($"{inputUserSimbol}{inputUserName}{inputUserSimbol}");
+                    continue;
+                }
+                for (int j = 0; j < inputUserName.Length + sizeNumberSimbol; j++)
+                {
+                    Console.Write(inputUserSimbol);
+
+                    if (j == inputUserName.Length + 1)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
